9586172c9e9998214dfb9838b77c8a85b117ae55
CalebTek <walexjackson@gmail.com>
2023-05-09 16:08:25 +0100

    updates

---
+++ GPA-Calculator/GPA-Calc/Authenticate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPA_Calc
+{
+    internal class Authenticate
+    {
+        // Field
+        Course[] _course { get; set; }
+        
+        // Constructor
+        public Authenticate(Course[] course) 
+        {
+            this._course = course;
+        }
+
+        // Method
+        public bool Exist(string courseCode)
+        {
+            bool exist = false;
+            foreach (Course course in _course) 
+            {
+                if (course != null && course.courseCode == courseCode)
+                {
+                    exist = true;
+                    break;
+                }
+            }
+            return exist;
+        }
+    }
+}

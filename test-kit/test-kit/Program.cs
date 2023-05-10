c876003a47ab1ac6b6ee6df6d696b4a79ed59a3c
CalebTek <walexjackson@gmail.com>
2023-05-10 16:42:44 +0100

    updates

---
+++ test-kit/test-kit/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Press a Key Y/N");
+ConsoleKeyInfo key = Console.ReadKey();
+Console.WriteLine();
+Console.WriteLine(key);
+Console.WriteLine(key.KeyChar);
+char iskey = (char)key.KeyChar;
+char iskey1 = (char)key.Key;
+Console.WriteLine(iskey);
+Console.WriteLine(iskey1);
+int keyint = (int)key.Key;
+Console.WriteLine(keyint);
+int keyint1 = (int)key.KeyChar;
+Console.WriteLine(keyint1);
+Console.WriteLine(keyint != 89);
+Console.ReadKey();

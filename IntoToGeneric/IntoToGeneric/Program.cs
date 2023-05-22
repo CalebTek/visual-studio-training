4111b3a926e4f75d62dd296fe5e5daea55d78696
CalebTek <walexjackson@gmail.com>
2023-05-22 10:08:33 +0100

    Create Intro to Generic

---
+++ IntoToGeneric/IntoToGeneric/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntoToGeneric
+{
+    namespace firstGeneric
+    {
+        internal class ProgramGeneric
+        {
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
